debeb431662fa6fbc72bdacd913615bc3286ea46
Ser_Lip <ser_lip@outlook.com>
2023-06-11 22:54:04 +0800

    add shell generate, fix iterations logic

--- vrun.py
+++ vrun.py
@@ -3,9 +3,12 @@ File: vrun.py
 Author: Ser_Lip
 Description: Ser_Lip's vcs command script
 TODO: >
-  vcs vlogan,vcs,sim opts logic
   yaml optimize
   regr parallel logic
+  result checker, not right
+  code coverage
+  dve wave, vdb
+  fsdb
   other optimize
 """
 
@@ -21,21 +24,28 @@ import datetime
 class seedGen(object):
   """
   An object that will generate a pseudo-random seed for test iterations.
-  Unused now!
-  """
-  def __init__(self, start_seed):
-    self.start_seed = start_seed
 
-  def get(self, test_iter=0):
-    return self.start_seed + test_iter
+  Not used now!
+  """
+  def __init__(self, startSeed):
+    self.startSeed = startSeed
+
+  def get(self, testIter=0):
+    return self.startSeed + testIter
 
   def getRand(self):
     return random.getrandbits(31)
 
 
-def parse_args(cwd):
+def parseArgs(cwd):
   """
   Create a command line parser.
+
+  Args:
+    cwd : current work directory.
+
+  Returns:
+    args : command line parser.
   """
   parser = argparse.ArgumentParser()
   parser.add_argument("-cfg", "--config", type=str,
@@ -86,9 +96,17 @@ def parse_args(cwd):
   return args
 
 
-def run_cmd(cmd, timeout_s=600, exit_on_error=True):
+def runCmd(cmd, tmoutSecond=600, exitOnError=True):
   """
   Run command with timeout limit and return output.
+
+  Args:
+    cmd : command to run.
+    tmoutSecond : timeout in second.
+    exitOnError : whether exit the script when command errors.
+
+  Return :
+    output : command output.
   """
   logging.info(cmd)
   try:
@@ -107,9 +125,9 @@ def run_cmd(cmd, timeout_s=600, exit_on_error=True):
     logging.debug("\nExited Ctrl-C from user request.")
     sys.exit(1)
   try:
-    output = ps.communicate(timeout=timeout_s)[0]
+    output = ps.communicate(timeout=tmoutSecond)[0]
   except subprocess.TimeoutExpired:
-    logging.error("Timeout[%ds]: %s"%(timeout_s, cmd))
+    logging.error("Timeout[%ds]: %s"%(tmoutSecond, cmd))
     output = ""
     ps.kill()
     sys.exit(1)
@@ -117,32 +135,32 @@ def run_cmd(cmd, timeout_s=600, exit_on_error=True):
   if rc and rc > 0:
     logging.debug(output)
     logging.error("Error return code: %s"%rc)
-    if exit_on_error:
+    if exitOnError:
       sys.exit(1)
   logging.debug(output)
   return output
 
 
-def read_yaml(yaml_file):
+def readYaml(yamlFile):
   """
   Read YAML file to a dictionary
 
   Args:
-    yaml_file : YAML file
+    yamlFile : YAML file
 
   Returns:
-    yaml_data : data read from YAML in dictionary format
+    yamlData : data read from YAML in dictionary format
   """
-  with open(yaml_file, "r") as f:
+  with open(yamlFile, "r") as f:
       try:
-          yaml_data = yaml.safe_load(f)
+          yamlData = yaml.safe_load(f)
       except yaml.YAMLError as exc:
           logging.error(exc)
           sys.exit(1)
-  return yaml_data
+  return yamlData
 
 
-def create_output(output, clean, prefix="out_"):
+def createOutput(output, clean, prefix="out_"):
   """
   Create output directory
 
@@ -165,23 +183,31 @@ def create_output(output, clean, prefix="out_"):
 
 
 flistCnt = 0
-def loadConfig(args, vcs_opts, test_list):
+def loadConfig(args, cfg, vcsOpts, testList):
   """
   Extract script config from YAML.
+
+  Args :
+    args : command line parser.
+    vcsOpts : vcs options data.
+    testList : test list extracted from YAML.
+
+  Returns :
+    Nothing
   """
-  yaml_data = read_yaml(args.cfg)
+  yamlData = readYaml(cfg)
   global flistCnt
-  for entry in yaml_data:
+  for entry in yamlData:
     if "import" in entry:
-      loadConfig(os.path.expandvars(entry['import']), vcs_opts, test_list)
+      loadConfig(args, os.path.expandvars(entry['import']), vcsOpts, testList)
     elif "vcs" in entry:
-      vcs_opts.update(entry)
+      vcsOpts.update(entry)
       flistCnt += 1
       if flistCnt > 1:
-        logging.error("Found more than 1 vcs_opts entry in config.")
+        logging.error("Found more than 1 vcsOpts entry in config.")
         sys.exit(1)
       if "flist" not in entry:
-        logging.error("Didn't find flist in vcs_opts.")
+        logging.error("Didn't find flist in vcsOpts.")
         sys.exit(1)
       # Compile options
       cmpOpts = ""
@@ -189,174 +215,214 @@ def loadConfig(args, vcs_opts, test_list):
         cmpOpts += " " + args.copt
       elif "cmp_opts" in entry:
         cmpOpts += " " + entry["cmp_opts"]
-      vcs_opts["cmp_opts"] = cmpOpts
+      vcsOpts["cmp_opts"] = cmpOpts
       # Elaboration options
       elabOpts = ""
       if args.eopt is not None:
         elabOpts += " " + args.eopt
       elif "elab_opts" in entry:
         elabOpts += " " + entry["elab_opts"]
-      vcs_opts["elab_opts"] = elabOpts
+      vcsOpts["elab_opts"] = elabOpts
     elif "test" in entry:
-      test_list.append(entry)
+      testList.append(entry)
 
 
-def get_env_var(var, debug_cmd=None):
-    """Get the value of environment variable
+def getEnvVar(var):
+  """
+  Get the value of environment variable
 
-    Args:
-      var : Name of the environment variable
+  Args:
+    var : Name of the environment variable
 
-    Returns:
-      val : Value of the environment variable
-    """
-    try:
-        val = os.environ[var]
-    except KeyError:
-        if debug_cmd:
-            return var
-        else:
-            logging.warning("Please set the environment variable {}".format(var))
-            sys.exit(1)
-    return val
+  Returns:
+    val : Value of the environment variable
+  """
+  try:
+    val = os.environ[var]
+  except KeyError:
+    logging.warning("Please set the environment variable {}".format(var))
+    sys.exit(1)
+  return val
 
 
-def load_regression_list(regr, regr_list):
+def loadRegrList(regr, regrList):
   """
   Extract regression list from file.
+
+  Args:
+    regr : regression list path.
+    regrList : test list extracted from regression list.
+
+  Returns :
+    Nothing
   """
-  yaml_data = read_yaml(regr)
-  for entry in yaml_data:
+  yamlData = readYaml(regr)
+  for entry in yamlData:
     if "test" in entry:
-      regr_list.append(entry)
+      regrList.append(entry)
 
 
-def gen(matched_list, vcs_opts, args, output_dir):
+def gen(matchedList, vcsOpts, args, outputDir):
   """
   Run the simulator.
+
+  Args:
+    matchedList : test to run.
+    vcsOpts : vcs options data.
+    args : command line parser.
+    outputDir : output directory.
+
+  Returns:
+    Nothing
   """
-  if args.co is False and len(matched_list) == 0:
+  if args.co is False and len(matchedList) == 0:
     return
-  vlogan_cmd = ("vlogan -full64 -sverilog -lca -ntb_opts uvm-1.2 -timescale=1ps/1ps "
-                "-l %s/compile/vlogan.log" % output_dir)
-  vcs_cmd = ("vcs -full64 -sverilog -lca -ntb_opts uvm-1.2 -partcomp "
+  vloganCmd = ("vlogan -full64 -sverilog -lca -ntb_opts uvm-1.2 -timescale=1ps/1ps "
+                "-l %s/compile/vlogan.log" % outputDir)
+  vcsCmd = ("vcs -full64 -sverilog -lca -ntb_opts uvm-1.2 -partcomp "
              "-CFLAGS '--std=c99 -fno-extended-identifiers' "
              "-LDFLAGS '-Wl,--no-as-needed' -debug_acc+all "
-             "-l %s/compile/vcs.log -o %s/compile/vcs.simv" % (output_dir, output_dir))
-  sim_cmd = "%s/compile/vcs.simv +UVM_VERBOSITY=%s" % (output_dir, args.v)
+             "-l %s/compile/vcs.log -o %s/compile/vcs.simv" % (outputDir, outputDir))
+  simCmd = "%s/compile/vcs.simv +UVM_VERBOSITY=%s" % (outputDir, args.v)
 
   if not args.so:
-    cmp_output = create_output(output_dir+"/compile", args.clean)
+    cmp_output = createOutput(outputDir+"/compile", args.clean)
     os.chdir(cmp_output)
     logging.info("------ Starting vlogan UVM ------")
-    run_cmd(vlogan_cmd, args.time)
-    vlogan_cmd = vlogan_cmd + " " + vcs_opts["flist"].strip("\n")
+    runCmd(vloganCmd, args.time)
+    vloganCmd = vloganCmd + " " + vcsOpts["flist"].strip("\n")
     logging.info("------ Starting vlogan ------")
-    run_cmd(vlogan_cmd, args.time)
+    with open("vlogan.sh", "w") as f:
+      f.write(vloganCmd)
+    runCmd(vloganCmd, args.time)
     logging.info("------ Finished ------")
-    vcs_cmd = vcs_cmd + " -top %s" % vcs_opts["top"].strip("\n")
+    vcsCmd = vcsCmd + " -top %s" % vcsOpts["top"].strip("\n")
     logging.info("------ Starting vcs ------")
-    run_cmd(vcs_cmd, args.time)
+    with open("vcs.sh", "w") as f:
+      f.write(vcsCmd)
+    runCmd(vcsCmd, args.time)
     logging.info("------ Finished ------")
   if not args.co:
-    for test in matched_list:
+    for test in matchedList:
       for i in range(test["iterations"]):
         if i != 0:
           test["seed"] = random.getrandbits(31)
-        sim_output = create_output(output_dir+"/"+test["test"]+"_%s"%test["seed"], args.clean)
-        os.chdir(sim_output)
-        sim_test_cmd = sim_cmd + "+UVM_TESTNAME=%s -l %s/sim.log" % (test["test"], sim_output)
+        simOutput = createOutput(outputDir+"/"+test["test"]+"_%s"%test["seed"], True)
+        os.chdir(simOutput)
+        simTestCmd = simCmd + "+UVM_TESTNAME=%s -l %s/sim.log" % (test["test"], simOutput)
         logging.info("------ Starting sim ------")
-        run_cmd(sim_test_cmd, args.time)
+        with open("sim.sh", "w") as f:
+          f.write(simTestCmd)
+        runCmd(simTestCmd, args.time)
+        with open("sim.log", "r") as f:
+          with open("error_message", "w") as em:
+            for line in f.readlines():
+              if "Error" or "error" or "UVM_ERROR" or "UVM_FATAL" in line:
+                em.write(line)
+          if os.path.getsize("error_message") == 0:
+            with open("PASS", "w") as fp:
+              fp.write("PASS")
+          else:
+            with open("FAIL", "w") as ff:
+              ff.write("FAIL")
         logging.info("------ Finished ------")
 
 
-def extractTest(args, test_list, matched_list):
+def extractTest(args, testList, matchedList):
   """
   extractTest for matched list.
+
+  Args:
+    args : command line parser.
+    testList : test list extracted from YAML.
+    matchedList : test to run.
+
+  Returns:
+    Nothing
   """
   if args.test is not None:
-    for entry in test_list:
+    for entry in testList:
       if entry["test"] == args.test:
-        matched_list.append(entry)
+        matchedList.append(entry)
         # Seed
         if args.seed is not None:
-          matched_list[-1]["seed"] = args.seed
+          matchedList[-1]["seed"] = args.seed
         elif "seed" in entry:
-          matched_list[-1]["seed"] = entry["seed"]
+          matchedList[-1]["seed"] = entry["seed"]
         else:
-          matched_list[-1]["seed"] = random.getrandbits(31)
+          matchedList[-1]["seed"] = random.getrandbits(31)
         # Iterations
-        if args.iter != 0:
-          matched_list[-1]["iterations"] = args.iter
+        if args.iter > 1:
+          matchedList[-1]["iterations"] = args.iter
         elif "iterations" in entry:
-          matched_list[-1]["iterations"] = entry["iterations"]
+          matchedList[-1]["iterations"] = entry["iterations"]
         else:
-          matched_list[-1]["iterations"] = args.iter
+          matchedList[-1]["iterations"] = args.iter
         # Simulation options
         simOpts = ""
         if args.sopt is not None:
           simOpts += " " + args.sopt
         elif "sim_opts" in entry:
           simOpts += " " + entry["sim_opts"]
-        matched_list[-1]["sim_opts"] = simOpts
+        matchedList[-1]["sim_opts"] = simOpts
   elif args.regr is not None:
-    regr_list = []
-    load_regression_list(args.regr, regr_list)
-    for entry in test_list:
-      for regr_entry in regr_list:
-        if entry["test"] == regr_entry["test"]:
-          matched_list.append(entry)
+    regrList = []
+    loadRegrList(args.regr, regrList)
+    for entry in testList:
+      for regrEntry in regrList:
+        if entry["test"] == regrEntry["test"]:
+          matchedList.append(entry)
           # Seed
           if args.seed is not None:
-            matched_list[-1]["seed"] = args.seed
-          elif "seed" in regr_entry:
-            matched_list[-1]["seed"] = regr_entry["seed"]
+            matchedList[-1]["seed"] = args.seed
+          elif "seed" in regrEntry:
+            matchedList[-1]["seed"] = regrEntry["seed"]
           elif "seed" in entry:
-            matched_list[-1]["seed"] = entry["seed"]
+            matchedList[-1]["seed"] = entry["seed"]
           else:
-            matched_list[-1]["seed"] = random.getrandbits(31)
+            matchedList[-1]["seed"] = random.getrandbits(31)
           # Iterations
-          if args.iter != 1:
-            matched_list[-1]["iterations"] = args.iter
-          elif "iterations" in regr_entry:
-            matched_list[-1]["iterations"] = regr_entry["iterations"]
+          if args.iter > 1:
+            matchedList[-1]["iterations"] = args.iter
+          elif "iterations" in regrEntry:
+            matchedList[-1]["iterations"] = regrEntry["iterations"]
           elif "iterations" in entry:
-            matched_list[-1]["iterations"] = entry["iterations"]
+            matchedList[-1]["iterations"] = entry["iterations"]
           else:
-            matched_list[-1]["iterations"] = args.iter
+            matchedList[-1]["iterations"] = args.iter
           # Simulation options
           simOpts = ""
           if args.sopt is not None:
             simOpts += " " + args.sopt
-          elif "sim_opts" in regr_entry:
-            simOpts += " " + regr_entry["sim_opts"]
+          elif "sim_opts" in regrEntry:
+            simOpts += " " + regrEntry["sim_opts"]
           elif "sim_opts" in entry:
             simOpts += " " + entry["sim_opts"]
-          matched_list[-1]["sim_opts"] = simOpts
+          matchedList[-1]["sim_opts"] = simOpts
 
 
 def main():
   """
-  This is the main entry point.
+  This is the main program.
   """
   try:
     #cwd = os.path.dirname(os.path.realpath(__file__))
     cwd = os.getcwd()
-    args = parse_args(cwd)
+    args = parseArgs(cwd)
     FORMAT = "%(asctime)-15s %(levelname)s: %(message)s"
     logging.basicConfig(format=FORMAT,level=logging.INFO)
     logging.info("Starting to run VCS script in cwd: %s" % cwd)
-    output_dir = create_output(args.o, args.clean)
+    logging.info("Output directory is %s" % os.path.expandvars(args.o))
+    outputDir = createOutput(args.o, args.clean)
 
-    vcs_opts = {}
-    test_list = []
-    loadConfig(args, vcs_opts, test_list)
+    vcsOpts = {}
+    testList = []
+    loadConfig(args, args.cfg, vcsOpts, testList)
 
-    matched_list = []
-    extractTest(args, test_list, matched_list)
+    matchedList = []
+    extractTest(args, testList, matchedList)
 
-    gen(matched_list, vcs_opts, args, output_dir)
+    gen(matchedList, vcsOpts, args, outputDir)
 
   except KeyboardInterrupt:
     logging.info("\nExited Ctrl+C from user request.")
