e0352ca3f0ee5157708e6a7d67d46017712d6527
Ser_Lip <ser_lip@outlook.com>
2023-07-06 23:35:12 +0800

    add cdr in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ export REPO_BASE=the/directory/of/repo
 
 alias vrun="python3 $VCS_RUN/vrun.py"
 alias mcdf="vrun -cfg $REPO_BASE/the/directory/of/yaml -o $REPO_BASE/out"
+alias cdr="cd $REPO_BASE"
 alias cdo="cd $REPO_BASE/out"
